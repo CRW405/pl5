7c27f5e06608a376e58a09d9b6963ef357fd894e
CRW405 <134819932+CRW405@users.noreply.github.com>
2026-04-06 15:01:42 -0500

    init

---
+++ lanalyzer.java
@@ -0,0 +1,25 @@
+public class lanalyzer {
+    public static void main(String[] args) {
+
+    }
+
+    int lookup(char c) {
+        return 0;
+    }
+
+    void addChar() {
+
+    }
+
+    void getChar() {
+
+    }
+
+    void getNonBlank() {
+
+    }
+
+    void lex() {
+
+    }
+}
